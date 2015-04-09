df0ac28655b689495944265069d4f8748c3cc16d
Kate Wareham <kaiaw25@gmail.com>
2015-04-09 17:38:48 -0400

    All project stuff!

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   
   # You can have the root of your site routed with "root"
-   root to: 'home#index'
+   root 'home#index'
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
     devise_for :users, controllers: {sessions: 'users/sessions', registrations: 'users/registrations'}
