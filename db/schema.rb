14ca27d4cbacb47f7e618b88b9a2e32647573193
Canon Brownell <canonbrownell@gmail.com>
2015-04-28 17:54:43 -0400

    Profile Picture

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150425172756) do
+ActiveRecord::Schema.define(version: 20150428212022) do
 
   create_table "listings", force: :cascade do |t|
     t.integer  "project_id"
